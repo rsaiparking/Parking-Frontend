c57f1d48ef8f2ccf885de5143ba563b853c265dd
rsaiparking <starstrucks1997@gmail.com>
2021-04-18 23:43:04 +0300

    Send email and plate number to the backend

--- src/components/upload-image.jsx
+++ src/components/upload-image.jsx
@@ -37,7 +37,36 @@ class ImageUploader extends Component {
                         </button>
                     </div>
                 </div>
+                <div className="col-md-4 mt-4 container card">
+                    <div className="text-center border border-light p-5" >
+                        <p className="h4 mb-4">Send information</p>
 
+                        <div className="form-group">
+                            <input
+                                name="email"
+                                type="text"
+                                className="form-control"
+                                id="emailInputField"
+                                aria-describedby="emailHelp"
+                                placeholder="Email"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <input
+                                name="plateNumber"
+                                type="text"
+                                className="form-control"
+                                id="plateNumberField"
+                                readOnly="readOnly"
+                            />
+                        </div>
+
+                        <button onClick={this.sendInformation} className="btn btn-info btn-block">
+                            Send information
+                        </button>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
@@ -60,7 +89,37 @@ class ImageUploader extends Component {
             console.log("Number " + jsonResponse['plateNumber']);
             const plateNumberH1 = document.getElementById('plateNumberText');
             plateNumberH1.textContent = jsonResponse['plateNumber'];
+            const plateNumberField = document.getElementById('plateNumberField');
+            plateNumberField.value = jsonResponse['plateNumber'];
         });
+    }
+
+    sendInformation = async () => {
+        const currentThis = this;
+        const email = document.getElementById('emailInputField').value;
+        const plateNumber = document.getElementById('plateNumberField').value;
+        if (email == undefined || email == null || email == "" ||
+        plateNumber == undefined || plateNumber == null || plateNumber == "") {
+            alert("Please extract your number and write down your email address!");
+            return;
+        }
+        const infoForm = {
+            email: email,
+            plateNumber: plateNumber
+        };
+        console.log(JSON.stringify(infoForm));
+        fetch("http://localhost:8080" + "/info", {
+            method: 'post',
+            body: JSON.stringify(infoForm),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(async response => {
+            await response.text();
+            if (response.status !== 200) {
+                alert('Information was not sent... Try again!');
+            }
+        })
     }
 
     changeInputField = event => {
