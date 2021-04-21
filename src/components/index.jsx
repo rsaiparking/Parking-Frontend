7300375546cdb669ce4724a02c9c59a7cb9bc371
marko517 <marko13431@gmail.com>
2021-04-21 23:29:15 +0300

    Get the parkings and display them

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,43 +3,48 @@ import React, {Component} from 'react';
 class Index extends Component {
 
     render() {
+      //I didnt manage to initialize the parkings variable
+      const parkings = [1, 2, 3];
         return (
             <React.Fragment>
                 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">Parking Name</th>
+      <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">{parkings[0]}</th>
+      <td>Centar</td>
+      <td><button type="button" onClick={this.getParkings} class="btn btn-success">Choose</button></td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">{parkings[1]}</th>
+      <td>Mladost 1</td>
+      <td><button type="button" onClick={this.getParkings} class="btn btn-success">Choose</button></td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">{parkings[2]}</th>
+      <td>Studentski Grad</td>
+      <td><button type="button" onClick={this.getParkings} class="btn btn-success">Choose</button></td>
     </tr>
   </tbody>
 </table>
-                <h1>IT WORKS</h1>
             </React.Fragment>
         );
     }
-   
+
+    //Calling the Backend
+    async getParkings() {
+      const response = await fetch("http://localhost:7777/parkings", {
+        method: 'GET'
+      });
+      const data = await response.json();
+      return data;
+    }
 }
 
 export default Index;
