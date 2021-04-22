38a9c65316199646e6582aa98ede8c2221a6d20a
marko517 <marko13431@gmail.com>
2021-04-22 23:54:18 +0300

    Loading HTML after response from BE

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,63 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class Index extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      parkingLots : []
+
     };
-}
-    render() {
-        return (
-            <React.Fragment>
-                <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Parking Name</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">{this.parkingLots}</th>
-      <td>Centar</td>
-      <td><button type="button" onClick={() => this.routeTo('/testPath/1') } className="btn btn-success">Choose</button></td>
-    </tr>
-    <tr>
-      <th scope="row">{this.parkingLots}</th>
-      <td>Mladost 1</td>
-      <td><button type="button" onClick={() => this.routeTo('/testPath/2') } className="btn btn-success">Choose</button></td>
-    </tr>
-    <tr>
-      <th scope="row">{this.parkingLots}</th>
-      <td>Studentski Grad</td>
-      <td><button type="button" onClick={() => this.routeTo('/testPath/3') } className="btn btn-success">Choose</button></td>
-    </tr>
-  </tbody>
-</table>
-            </React.Fragment>
-        );
-    }
+  }
 
-    async getParkings() {
-      const response = await fetch("http://localhost:7777/parkings", {
-        method: 'GET'
-      });
-      const data = await response.json();
-      console.log(data);
-      return data;
-    }
+  componentDidMount() {
+    this.getParkings();
+  }
 
-    routeTo(path) {
-      this.props.history.push(path);
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div id="parking" class="row text-center"></div>
+      </React.Fragment>
+    );
+  }
 
-    async componentDidMount() {
-      const current = this;
-      this.setState({parkingLots: await current.getParkings()});
-    }
+  async getParkings() {
+    let elements = [];
+    const response = await fetch("http://localhost:7777/parkings", {
+      method: 'GET'
+    });
+    const data = await response.json();
+    const projectElement = (<table className="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Parking Name</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">{data[0]}</th>
+          <td>Centar</td>
+          <td><button type="button" onClick={() => this.routeTo('/testPath/1')} className="btn btn-success">Choose</button></td>
+        </tr>
+        <tr>
+          <th scope="row">{data[1]}</th>
+          <td>Mladost 1</td>
+          <td><button type="button" onClick={() => this.routeTo('/testPath/2')} className="btn btn-success">Choose</button></td>
+        </tr>
+        <tr>
+          <th scope="row">{data[2]}</th>
+          <td>Studentski Grad</td>
+          <td><button type="button" onClick={() => this.routeTo('/testPath/3')} className="btn btn-success">Choose</button></td>
+        </tr>
+      </tbody>
+    </table>);
+    elements.push(projectElement);
+    const parkingDiv = document.getElementById('parking');
+    ReactDOM.render(elements, parkingDiv);
+  }
+
+  routeTo(path) {
+    this.props.history.push(path);
+  }
 }
 
 export default Index;
