b9972121eeac00020e07242814df76728a27089b
marko517 <marko13431@gmail.com>
2021-04-22 22:29:15 +0300

    Adding parking loading

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,11 +2,16 @@ import React, {Component} from 'react';
 
 class Index extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      parkingLots : []
+    };
+}
     render() {
-      const parkings = [1, 2, 3];
         return (
             <React.Fragment>
-                <table class="table">
+                <table className="table">
   <thead>
     <tr>
       <th scope="col">ID</th>
@@ -16,19 +21,19 @@ class Index extends Component {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">{parkings[0]}</th>
+      <th scope="row">{this.parkingLots}</th>
       <td>Centar</td>
-      <td><button type="button" onClick={() => this.routeTo('/testPath') } class="btn btn-success">Choose</button></td>
+      <td><button type="button" onClick={() => this.routeTo('/testPath/1') } className="btn btn-success">Choose</button></td>
     </tr>
     <tr>
-      <th scope="row">{parkings[1]}</th>
+      <th scope="row">{this.parkingLots}</th>
       <td>Mladost 1</td>
-      <td><button type="button" onClick={() => this.routeTo('/testPath') } class="btn btn-success">Choose</button></td>
+      <td><button type="button" onClick={() => this.routeTo('/testPath/2') } className="btn btn-success">Choose</button></td>
     </tr>
     <tr>
-      <th scope="row">{parkings[2]}</th>
+      <th scope="row">{this.parkingLots}</th>
       <td>Studentski Grad</td>
-      <td><button type="button" onClick={() => this.routeTo('/testPath') } class="btn btn-success">Choose</button></td>
+      <td><button type="button" onClick={() => this.routeTo('/testPath/3') } className="btn btn-success">Choose</button></td>
     </tr>
   </tbody>
 </table>
@@ -41,11 +46,17 @@ class Index extends Component {
         method: 'GET'
       });
       const data = await response.json();
+      console.log(data);
       return data;
     }
 
     routeTo(path) {
       this.props.history.push(path);
+    }
+
+    async componentDidMount() {
+      const current = this;
+      this.setState({parkingLots: await current.getParkings()});
     }
 }
 
